ff7a66c91ea936e6344b372f5fababede8d3e085
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-06-03 16:27:14 +0700

    hot fix

--- HuloToys-Service/Controllers/Product/ProductController.cs
+++ HuloToys-Service/Controllers/Product/ProductController.cs
@@ -181,11 +181,11 @@ namespace WEB.CMS.Controllers
         [HttpPost("detail")]
         public async Task<IActionResult> ProductDetail([FromBody] APIRequestGenericModel input)
         {
-            var model_con = new
-            {
-                id = "68254f39711071e30c18badb"
-            };
-            input.token = CommonHelper.Encode(JsonConvert.SerializeObject(model_con), _configuration["KEY:private_key"]);
+            //var model_con = new
+            //{
+            //    id = "68254f39711071e30c18badb"
+            //};
+            //input.token = CommonHelper.Encode(JsonConvert.SerializeObject(model_con), _configuration["KEY:private_key"]);
             try
             {
                 JArray objParr = null;

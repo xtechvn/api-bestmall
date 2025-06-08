a8e4734ed969f6883dd9a0999ca75225d8c8c24d
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-06-08 14:37:27 +0700

    fix api flashsale

--- HuloToys-Service/Controllers/Flashsale/Bussiness/FlashsaleService.cs
+++ HuloToys-Service/Controllers/Flashsale/Bussiness/FlashsaleService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.ViewModels.Products;
+using HuloToys_Service.Controllers.Product.Bussiness;
 using HuloToys_Service.Models.Flashsale;
 using Utilities.Contants;
 
@@ -7,41 +8,28 @@ namespace HuloToys_Service.Controllers.Flashsale.Bussiness
     public class FlashsaleService
     {
         private readonly IConfiguration _configuration;
+        private readonly ProductDetailService productDetailService;
+
         public FlashsaleService(IConfiguration configuration)
         {
             _configuration = configuration;
+            productDetailService=new ProductDetailService(configuration);
         }
-        public FlashSaleProductResposeModel CombineModel(FlashSaleProductESModel flashsale_product, ProductMongoDbModel product)
+        public FlashSaleProductResposeModel CombineModel(FlashSaleProductESModel flashsale_product, ProductMongoDbModelFEResponse product)
         {
             if (flashsale_product == null|| product==null)
             {
                 return null;
 
             }
-            double total_discount = 0;
-            var amount = (product.amount_min != null && product.amount_min > 0) ? (double)product.amount_min : product.amount;
-            switch (flashsale_product.valuetype)
-            {
-                case (int)FlashSaleValueType.VND:
-                    {
-                        total_discount = flashsale_product.discountvalue==null?0: (double)flashsale_product.discountvalue;
-                    }break;
-                case (int)FlashSaleValueType.PERCENT:
-                    {
-                        var percent = flashsale_product.discountvalue == null ? 0 : (double)flashsale_product.discountvalue;
-                        
-                        total_discount=amount*percent/(double)100;
-                        if (total_discount > 0) total_discount = Math.Round(total_discount, 0);
-                    }
-                    break;
-            }
+           
             return new FlashSaleProductResposeModel()
             {
-                amount = amount,
-                amount_after_flashsale = amount- total_discount,
+                amount = ((product.amount_min != null && product.amount_min > 0) ? (double)product.amount_min : product.amount),
+                amount_after_flashsale = product.amount_after_flashsale,
                 discountvalue = flashsale_product.discountvalue,
                 position = flashsale_product.position,
-                total_discount = total_discount,
+                total_discount = product.discount,
                 valuetype = flashsale_product.valuetype,
                 _id = product._id,
                 avatar = product.avatar,

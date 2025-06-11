8ec0aba4cb8240849cc3fac6f7a4b29475b43379
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-06-11 09:01:15 +0700

    hotfix

--- HuloToys-Service/Controllers/News/NewsController.cs
+++ HuloToys-Service/Controllers/News/NewsController.cs
@@ -579,16 +579,16 @@ namespace HuloToys_Service.Controllers
         [HttpPost("get-list-by-categoryid-order.json")]
         public async Task<ActionResult> getListArticleByCategoryIdOrderByDate([FromBody] APIRequestGenericModel input)
         {
-            var input_model = new
-            {
-                category_id = 22,
-                skip = 0,
-                take = 10
-            };
-            input = new APIRequestGenericModel()
-            {
-                token = CommonHelper.Encode(JsonConvert.SerializeObject(input_model), configuration["KEY:private_key"])
-            };
+            //var input_model = new
+            //{
+            //    category_id = 22,
+            //    skip = 0,
+            //    take = 10
+            //};
+            //input = new APIRequestGenericModel()
+            //{
+            //    token = CommonHelper.Encode(JsonConvert.SerializeObject(input_model), configuration["KEY:private_key"])
+            //};
             try
             {
                 JArray objParr = null;

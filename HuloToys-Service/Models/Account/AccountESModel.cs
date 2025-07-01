ee1fe20984284d4f9a7d1f8bd71edce53b13342a
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-01 15:41:43 +0700

    fix model

--- HuloToys-Service/Models/Account/AccountESModel.cs
+++ HuloToys-Service/Models/Account/AccountESModel.cs
@@ -27,6 +27,7 @@ namespace HuloToys_Service.Models.Account
         public byte? Status { get; set; }
         [PropertyName("GroupPermission")]
         public int? GroupPermission { get; set; }
+        [PropertyName("GoogleToken")]
         public string? GoogleToken { get; set; }
 
     }

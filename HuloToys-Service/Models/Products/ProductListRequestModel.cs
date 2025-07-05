a4d100e55b4efe106744d52187d56e934986a155
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-05 09:30:26 +0700

    update api

--- HuloToys-Service/Models/Products/ProductListRequestModel.cs
+++ HuloToys-Service/Models/Products/ProductListRequestModel.cs
@@ -25,6 +25,7 @@
     public class ProductFlashSaleByTypeRequestModel
     {
         public int? type { get; set; }
+        public int? group_id { get; set; }
         public int page_index { get; set; }
         public int page_size { get; set; }
     }

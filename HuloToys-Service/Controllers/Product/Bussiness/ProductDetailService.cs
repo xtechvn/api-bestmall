bf10713b729e9a9966fd9e4fe414769d9008ced2
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-15 10:16:06 +0700

    log tele

--- HuloToys-Service/Controllers/Product/Bussiness/ProductDetailService.cs
+++ HuloToys-Service/Controllers/Product/Bussiness/ProductDetailService.cs
@@ -313,7 +313,7 @@ namespace HuloToys_Service.Controllers.Product.Bussiness
                         var selected = list_product_mongo.FirstOrDefault(x => x._id == item.productid);
                         if (selected == null || selected._id == null || selected.status!=1 || selected.supplier_status!=1)
                         {
-                            LogHelper.InsertLogTelegram("GetFlashSaleProductByProductIds Ignore [" + (selected == null ? "NULL" : selected._id) +"] ["+ (selected == null ? "NULL" : selected.status) + "]["+ (selected == null ? "NULL" : selected.supplier_status) + "]");
+                            LogHelper.InsertLogTelegram("GetFlashSaleProductByProductIds Ignore [ID=" + (selected == null ? "NULL" : selected._id) +"] [supplier_id="+ (selected == null ? "NULL" : selected.supplier_id) + "][status="+ (selected == null ? "NULL" : selected.status) + "][supplier_status="+ (selected == null ? "NULL" : selected.supplier_status) + "]");
                             continue;
                         }
                         var amount_product = selected.amount;

aa7bc1ef9c9bb887574a94ea329a400eee230d3e
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-15 15:50:20 +0700

    Update EmailService.cs

--- HuloToys-Service/Controllers/Client/Business/EmailService.cs
+++ HuloToys-Service/Controllers/Client/Business/EmailService.cs
@@ -69,6 +69,8 @@ namespace HuloToys_Service.Controllers.Client.Business
                         .Replace("{client_name}",client.ClientName)
                         .Replace("{username}", account.UserName)
                         .Replace("{change_password_url}", forgot_password_token.Replace("+", "-").Replace("/", "_"))
+                        .Replace("/tai-khoan/doi-mat-khau/", "/quen-mat-khau/tai-khoan/doi-mat-khau/token=")
+
                         ;
                     message.Body = body_fixed;
                     message.IsBodyHtml = true;

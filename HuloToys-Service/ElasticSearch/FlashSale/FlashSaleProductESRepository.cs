e410c6455f77c328f3e7765b4b57828426122ae3
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-17 17:16:09 +0700

    Update FlashSaleProductESRepository.cs

--- HuloToys-Service/ElasticSearch/FlashSale/FlashSaleProductESRepository.cs
+++ HuloToys-Service/ElasticSearch/FlashSale/FlashSaleProductESRepository.cs
@@ -234,7 +234,34 @@ namespace Caching.Elasticsearch.FlashSale
 
             // Calculate 'from' (skip) based on page_index and page_size
             var from = (page_index - 1) * page_size;
+            if (group_id < 0 && type < 0)
+            {
+                var response_all = await _client.SearchAsync<FlashSaleProductESModel>(s => s
+                  .Query(q => q
+                      .Bool(b =>
+                       b.Must(m =>  m.Term(t => t
+                                        .Field(f => f.status)
+                                        .Value(1)
+                                    ),
 
+                                    m => m.Terms(t => t
+                                        .Field(f => f.flashsale_id)
+                                        .Terms(flashsale_ids)
+                                    ))
+                       )
+                  )
+                  .From(from)
+                  .Size(page_size)
+                );
+                if (response_all.IsValid)
+                {
+                    return response_all.Documents.ToList();
+                }
+                else
+                {
+                    return new List<FlashSaleProductESModel>();
+                }
+            }
             var response = await _client.SearchAsync<FlashSaleProductESModel>(s => s
                 .Query(q => q
                     .Bool(b =>

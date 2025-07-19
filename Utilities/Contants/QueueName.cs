773f45f4f9660cc0b56b0025fd6dcfcbd203a0d9
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-19 15:30:15 +0700

    Update QueueName.cs

--- Utilities/Contants/QueueName.cs
+++ Utilities/Contants/QueueName.cs
@@ -3,6 +3,6 @@
     public class QueueName
     {
         public const string queue_app_push = "queue_app_push_hulotoys";
-        public const string QUEUE_CHECKOUT = "queue_checkout_order_hulotoys";
+        public const string QUEUE_CHECKOUT = "queue_checkout_order";
     }
 }

67e8c3e3a2c304a2872b074d9a262766b4fc60f1
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-24 08:20:35 +0700

    update

---
+++ HuloToys-Service/Models/HomepageBannerModel.cs
@@ -0,0 +1,10 @@
+﻿using HuloToys_Service.Models.Models;
+
+namespace HuloToys_Service.Models
+{
+    public class HomepageBannerModel
+    {
+        public List<AllCode> main { get; set; }
+        public List<AllCode> sub { get; set; }
+    }
+}

8630d5c13ca386bc1c2a1148f4149c64e0671f4d
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-07-24 15:42:01 +0700

    update

--- HuloToys-Service/Controllers/Product/ProductController.cs
+++ HuloToys-Service/Controllers/Product/ProductController.cs
@@ -1068,7 +1068,9 @@ namespace WEB.CMS.Controllers
                                 label.Icon,
                                 label.Banner,
                                 label.Description,
-                                label.Avatar
+                                label.Avatar,
+                                label.BannerMain,
+                                label.BannerSub
 
                             }
                         }

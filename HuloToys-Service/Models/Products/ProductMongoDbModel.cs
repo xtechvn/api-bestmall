3bbed9f014d541cd8fd03f2248eadf9a08a222a3
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-08-01 20:49:50 +0700

    update

--- HuloToys-Service/Models/Products/ProductMongoDbModel.cs
+++ HuloToys-Service/Models/Products/ProductMongoDbModel.cs
@@ -78,11 +78,12 @@ namespace Entities.ViewModels.Products
     {
         public int? exists_flashsale_id { get; set; }
         public int? flashsale_badge_type { get; set; }
-        
         public string exists_flashsale_name { get; set; }
         public double? amount_after_flashsale { get; set; }
         public DateTime? flash_sale_fromdate { get; set; }
         public DateTime? flash_sale_todate { get; set; }
+        public string supplier_name { get; set; } = null;
+
     }
     public class ProductMongoDbModelFEResponseCollection : ProductMongoDbModelFEResponse
     {

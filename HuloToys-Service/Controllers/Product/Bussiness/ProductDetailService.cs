b818ce9cf9fa9545c46a0b5ddfeabca8ed71d85d
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-08-06 09:59:47 +0700

    Update ProductDetailService.cs

--- HuloToys-Service/Controllers/Product/Bussiness/ProductDetailService.cs
+++ HuloToys-Service/Controllers/Product/Bussiness/ProductDetailService.cs
@@ -310,12 +310,11 @@ namespace HuloToys_Service.Controllers.Product.Bussiness
 
                     foreach (var selected in list_product_mongo)
                     {
-                        // var selected = list_product_mongo.FirstOrDefault(x => x._id == item.productid);
-                        //if (selected == null || selected._id == null || selected.status!=1 || selected.supplier_status!=1)
-                        //{
-                        //   // LogHelper.InsertLogTelegram("GetFlashSaleProductByProductIds Ignore [ID=" + (selected == null ? "NULL" : selected._id) +"] [supplier_id="+ (selected == null ? "NULL" : selected.supplier_id) + "][status="+ (selected == null ? "NULL" : selected.status) + "][supplier_status="+ (selected == null ? "NULL" : selected.supplier_status) + "]");
-                        //    continue;
-                        //}
+                        ProductMongoDbModelFEResponse extend_product = new ProductMongoDbModelFEResponse()
+                        {
+                            _id=selected._id,
+                        };
+                        UpdateProductRaiting(extend_product);
                         var item = list_item.First(x => x.productid.Trim()==selected._id.Trim());
                         var amount_product = selected.amount;
                         if (selected.amount <= 0 && selected.amount_min != null && selected.amount_min > 0)
@@ -354,8 +353,8 @@ namespace HuloToys_Service.Controllers.Product.Bussiness
                             avatar = selected.avatar,
                             name = selected.name,
                             code = selected.code,
-                            rating = selected.rating,
-                            review_count = selected.review_count,
+                            rating = extend_product.rating,
+                            review_count = extend_product.review_count,
 							total_sold = selected.total_sold,
                             super_sale=item.supersale,
                             badge_type=item.badgetype

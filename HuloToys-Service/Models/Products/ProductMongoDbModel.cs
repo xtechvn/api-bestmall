d9033c8f9375a5b8df281361d59c9329a0bf947f
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-08-10 14:31:55 +0700

    update

--- HuloToys-Service/Models/Products/ProductMongoDbModel.cs
+++ HuloToys-Service/Models/Products/ProductMongoDbModel.cs
@@ -69,7 +69,10 @@ namespace Entities.ViewModels.Products
         public string description_delivery { get; set; }
         public string description_refund { get; set; }
 
-
+        public List<string>? attachment_root { get; set; }
+        public List<string>? attachment_product { get; set; }
+        public List<string>? attachment_supply { get; set; }
+        public List<string>? attachment_confirm { get; set; }
         public int? flashsale_badge_type { get; set; }
 
     }

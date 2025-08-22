5fe1a9c4a64e4da48f41cc1fe41aad03c84e4d43
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-08-22 12:05:50 +0700

    Update VoucherController.cs

--- HuloToys-Service/Controllers/VOUCHER/VoucherController.cs
+++ HuloToys-Service/Controllers/VOUCHER/VoucherController.cs
@@ -443,8 +443,7 @@ namespace API_CORE.Controllers.VOUCHER
                 if (list_global == null || list_global.Count <= 0)
                 {
                     list_global = await voucherRepository.GetVoucherList(null, 1, 1, 50, null);
-                    LogHelper.InsertLogTelegram("GetVoucherList - VoucherController - list_global:  " + JsonConvert.SerializeObject(list_global));
-                    list_global = list_global.Where(x => (x.group_user_priority == null || x.group_user_priority.Trim() == "") && x.limitUse > 0).ToList();
+                    list_global = list_global.Where(x => (x.group_user_priority == null || x.group_user_priority.Trim() == ""|| x.group_user_priority.Trim() == "[]") && x.limitUse > 0).ToList();
                     if (list_global != null && list_global.Count > 0)
                     {
                         int db_index = Convert.ToInt32(configuration["Redis:Database:db_search_result"].ToString());
@@ -457,8 +456,6 @@ namespace API_CORE.Controllers.VOUCHER
                 if (list == null || list.Count <= 0)
                 {
                     list = await voucherRepository.GetVoucherList(null, 1, 1, 100, (long)account_client.ClientId);
-                    LogHelper.InsertLogTelegram("GetVoucherList - VoucherController - list:  " + JsonConvert.SerializeObject(list));
-
                     list = list.Where(x => x.group_user_priority != null && x.group_user_priority.Trim() != "" && x.limitUse > 0).ToList();
                     if (list != null && list.Count > 0)
                     {

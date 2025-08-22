eb49c04820abc612dbb71a0b1737ff9ada730982
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-08-22 11:22:50 +0700

    Update ProductDetailMongoAccess.cs

--- HuloToys-Service/MongoDb/ProductDetailMongoAccess.cs
+++ HuloToys-Service/MongoDb/ProductDetailMongoAccess.cs
@@ -511,5 +511,26 @@ namespace HuloToys_Service.MongoDb
                 return new List<ProductMongoDbModel>();
             }
         }
+        public async Task<long> CountListByProducts(List<string> ids)
+        {
+            try
+            {
+                var filter = Builders<ProductMongoDbModel>.Filter;
+                var filterDefinition = filter.Empty;
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.In(x => x._id, ids);
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(x => x.status, (int)ProductStatus.ACTIVE);
+                filterDefinition &= Builders<ProductMongoDbModel>.Filter.Eq(p => p.supplier_status, (int)SUPPLIER_STATUS.CONFIRMED);
+
+                var model = _productDetailCollection.CountDocumentsAsync(filterDefinition);
+                var result = await model;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                LogHelper.InsertLogTelegram("CountListByProducts err=" + ex.ToString());
+
+            }
+            return 0;
+        }
     }
 }

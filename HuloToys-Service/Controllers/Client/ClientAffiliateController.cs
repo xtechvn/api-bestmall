4bc45da5cb30e2095df8933fc583a427e42beb52
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-08-28 14:20:03 +0700

    Update ClientAffiliateController.cs

--- HuloToys-Service/Controllers/Client/ClientAffiliateController.cs
+++ HuloToys-Service/Controllers/Client/ClientAffiliateController.cs
@@ -129,7 +129,7 @@ namespace HuloToys_Service.Controllers
                             { "store_name", "SP_GetClient" },
                             { "index_es", "hulotoys_sp_getclient"},
                             { "project_type", 1 },
-                            { "id", client_sql.Id}
+                            { "id", -1}
                         };
 
                         var _data_push = JsonConvert.SerializeObject(j_param);

3a0bb140453efa619337119dd42ee1bb24d117aa
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-09-03 15:57:55 +0700

    update

--- HuloToys-Service/IRepositories/IBankingAccountRepository.cs
+++ HuloToys-Service/IRepositories/IBankingAccountRepository.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using DAL;
+using Entities.Models;
 using HuloToys_Service.Models.Models;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,7 @@ namespace Repositories.IRepositories
         BankingAccount GetById(int bankAccountId);
         List<BankingAccount> GetBankAccountByClientId(long clientId);
         public int UpsertBankingAccount(BankingAccount model);
+        public int Insert(BankingAccount model);
+        public int Update(BankingAccount model);
     }
 }

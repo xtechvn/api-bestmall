cbaf6c268e4095953f5ce72a135129883dd710c8
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-09-06 15:35:42 +0700

    Update OrderController.cs

--- HuloToys-Service/Controllers/Order/OrderController.cs
+++ HuloToys-Service/Controllers/Order/OrderController.cs
@@ -833,9 +833,10 @@ namespace HuloToys_Service.Controllers
                     model.utm_source = request.utm_source;
                   
                     var result = await orderMongodbService.Insert(model);
+                  
+                    var exists= await orderMongodbService.GetByOrderNo(model.order_no);
                     LogHelper.InsertLogTelegramByUrl(configuration["BotSetting:bot_token"], configuration["BotSetting:bot_group_id"],
-                      "Order/Confirm 2:[" + model._id + "][" + request.utm_source + "][" + request.utm_medium + "] [" + model.utm_source + "][" + model.utm_medium + "]");
-
+                    "Order/Confirm 2:[" + model._id + "][" + model.utm_source + "][" + model.utm_medium + "] Exists: [" + exists.utm_source + "][" + exists.utm_medium + "]");
                     //-- Insert Queue:
                     var queue_model = new CheckoutQueueModel() { 
                         event_id = (int)CheckoutEventID.CREATE_ORDER, 

784652128bcc2db9cd2e5b1e4f981a8b25b58ec7
Kagami Kyouko <34211884+Mirrors1508@users.noreply.github.com>
2025-09-06 09:10:29 +0700

    Update ClientAffiliateController.cs

--- HuloToys-Service/Controllers/Client/ClientAffiliateController.cs
+++ HuloToys-Service/Controllers/Client/ClientAffiliateController.cs
@@ -570,18 +570,13 @@ namespace HuloToys_Service.Controllers
                     if (client != null && client.IsRegisterAffiliate == true && client.ReferralId != null && client.ReferralId.Trim() != "")
                     {
                         var result =   _allotmentFundRepository.GetByAccountClientId(account_client_id);
-                        if (result != null && result.Id>0)
+                        if (result == null || result.Id<=0)
                         {
-                            return Ok(new
-                            {
-                                status = (int)ResponseType.SUCCESS,
-                                msg = "Success",
-                                data = result,
-                            });
+                           
                         }
                         else
                         {
-                           var fund = new HuloToys_Service.Models.Models.AllotmentFund()
+                            result = new HuloToys_Service.Models.Models.AllotmentFund()
                             {
                                 UpdateTime = DateTime.Now,
                                 AccountBalance = 0,
@@ -590,9 +585,14 @@ namespace HuloToys_Service.Controllers
                                 FundType = 1,
 
                             };
-                            fund.Id = _allotmentFundRepository.Insert(fund);
+                            result.Id = _allotmentFundRepository.Insert(result);
                         }
-
+                        return Ok(new
+                        {
+                            status = (int)ResponseType.SUCCESS,
+                            msg = "Success",
+                            data = result,
+                        });
                     }
                  
                     return Ok(new
